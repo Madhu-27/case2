c18016680193e29f48a2ab5b3dacdcd444d85516
Madhu-27 <mailmadhu2596@gmail.com>
2020-04-26 14:57:12 +0530

    comments

---
+++ src/test/java/test_runner/test_runner.java
@@ -0,0 +1,14 @@
+package test_runner;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue= {"test_definitionB"}
+		)
+public class test_runner {
+
+}
